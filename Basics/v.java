c33ac4ae44d70be8db5b2e729898eb6a76d9eba8
0xsadik <torikussadik404@gmail.com>
2024-08-27 22:27:21 +0600

    .

---
+++ Basics/v.java
@@ -0,0 +1,14 @@
+public class v {
+    public static void main(String[] args){ 
+        String name = "Torikus";
+        int age = 24;
+        float cgpa = 3.68f;
+        char grade = 'A';
+        boolean isPass  = true;
+        System.out.println("Person's name : " + name);
+        System.out.println("Person's age : " + age);
+        System.out.println("Person's cGPA : " + cgpa);
+        System.out.println("Person's Grade : " + grade);
+        System.out.println("Person is pass ? : " + isPass);
+    }
+}
