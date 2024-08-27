b865e0ff6713a4c0edf378e1fe51e1051be25f9d
0xsadik <torikussadik404@gmail.com>
2024-08-27 22:37:23 +0600

    .

---
+++ 2. DataTypes/dtype.java
@@ -0,0 +1,38 @@
+/**
+ 
+ * Data type         size          
+    byte            1 byte        
+    short           2 bytes
+    int             4 bytes
+    long            8 bytes
+    float           4 bytes
+    double          8 bytes
+    boolean         1 bit
+    char            2 bytes
+
+*/
+
+public class dtype {
+    public static void main(String[] args) {
+
+        // byte 
+        byte num = 100;
+        // System.out.println(num);
+
+        // short 
+        short snum = 5000;
+        // System.out.println(snum);
+
+        // int 
+        int i_num = 100000;
+        // System.out.println(i_num);
+
+        // long
+        long l_num = 15000000000L;
+        // System.out.println(l_num);
+
+        // float 
+        float f_num = 5.796f;
+        System.out.println(f_num);
+    }
+}
