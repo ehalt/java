d4389b4e38480d86ad9e506608c395a40fedcbf0
0xsadik <torikussadik404@gmail.com>
2024-08-27 23:12:51 +0600

    .

---
+++ 5. Strings/concat.java
@@ -0,0 +1,12 @@
+public class concat {
+    public static void main(String[] args) {
+        String first_name = "Shakil";
+        String last_name = "Babu";
+
+        // System.out.println(first_name + " " + last_name);
+
+        String p1 = "This is a beautiful";
+        String p2 = " place to visit.";
+        System.out.println(p1.concat(p2));
+    }
+}
---
+++ 5. Strings/str.java
@@ -0,0 +1,19 @@
+public class str {
+    public static void main(String[] args) {
+        // string length 
+        String txt =  "yaaad main rakhnaa";
+        // System.out.println("Length of txt: " + txt.length());
+
+
+        // toUpperCase() 
+        // System.out.println(txt.toUpperCase());
+
+        // -> toLowerCase()
+        String name = "A. HALIM KHAN";
+        // System.out.println(name.toLowerCase());
+
+        // -> finding a character in a string 
+        String lname = "Sadik";
+        // System.out.println(lname.indexOf("dik"));
+    }
+}
