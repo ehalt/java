601e9d7d3032664aa7914cb73e247097235b5931
ehalt <torikussadikk@gmail.com>
2024-08-29 15:56:18 +0600

    .

---
+++ Array/string_arr.java
@@ -0,0 +1,8 @@
+public class string_arr {
+    public static void main(String[] args) {
+        String[] veg = {"tomato", "potato", "onion", "garlic"};
+        for (int i = 0; i < veg.length; i++) {
+            System.out.println(veg[i]);
+        }
+    }
+}
