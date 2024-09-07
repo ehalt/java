61fd28a7a3217a30de0fc5bf5dd34f6f47c36a4e
0xsadik <torikussadik404@gmail.com>
2024-09-07 12:28:54 +0600

    .

---
+++ Array/prac.java
@@ -0,0 +1,26 @@
+// public class prac {
+
+//     static void hello() {
+//         System.out.println("Hello, there!!");
+//     }
+
+//     public static void main(String[] args) {
+//         hello();
+//         hello();
+//         hello();
+//         hello();
+//     }
+// }
+
+
+// public class prac {
+//     static void his_name(String name) {
+//         System.out.println("His name is : " + name);
+//     }
+    
+//     public static void main(String[] args) {
+//         his_name("Torikus!");
+//     }
+// }
+
+
