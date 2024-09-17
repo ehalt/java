71ce652b7f6b8841b797bdbee26d2e6d0d81eaa2
0xsadik <torikussadik404@gmail.com>
2024-09-17 12:55:00 +0600

    .

--- ts.js
+++
@@ -1,8 +0,0 @@
-function solve(x, m, d){
-    let idc = x * m
-    let idk = x + d
-    let Min = Math.min(idc, idk)
-    console.log(Min)
-}
-solve(1, 3, 1);
-
--- ts2.js
+++
@@ -1,6 +0,0 @@
-
-function uttor(x1, x2, y1, y2, z1, z2) {
-    console.log((x1 <= x2 && y1 <= y2 && z1 >= z2) ? "YES" : "NO");
-}
-uttor(20, 50, 2100, 1900, 6, 6);
-uttor(27, 27, 1920, 1920, 3, 3);
